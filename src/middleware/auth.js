7fd5edb8d5f6d869fafe79354f11dff320c573d8
YAO <2908724126@qq.com>
2026-05-01 15:19:51 +0800

    fix(auth): protect dashboard route

---
+++ src/middleware/auth.js
@@ -0,0 +1,9 @@
+function requireAuth(req, res, next) {
+    if (req.session && req.session.user) {
+        return next();
+    }
+
+    return res.redirect("/signup");
+}
+
+module.exports = requireAuth;
